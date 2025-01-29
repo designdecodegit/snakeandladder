8101085e4c98125b2fbe18710cc4a058750cf788
designdecodegit <designdecodechannel@gmail.com>
2025-01-29 14:50:35 +0530

    Added Description

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 
 int main(){
+    //Get Board Size
     int boardSize;
     do {
         cout<<"What is the size of board you want in nxn format (between 2 and 10)!"<<endl;
@@ -22,6 +23,7 @@ int main(){
 
     int boardMaxSize = boardSize * boardSize;
     
+    //Get no of players
     int playerNo;
     do {
         cout<<"How many players you want to start the game with?"<<endl;
@@ -33,6 +35,7 @@ int main(){
         }
     } while(playerNo < 2);
 
+    //Get player details
     string name;
     queue<Player> players;
     for (int i = 0; i < playerNo; i++)
@@ -44,6 +47,7 @@ int main(){
 
     vector<Player> winners;
 
+    //Get dice count
     int diceCount;
     do {
         cout << "How many dice do you want to use?" << endl;
@@ -54,6 +58,7 @@ int main(){
         }
     } while (diceCount < 1);
 
+    //Get no of snakes and their details
     int snakeSize;
     vector<Snake> snakeList;
     cout<<"How many snakes you want in your board?"<<endl;
@@ -70,6 +75,7 @@ int main(){
         snakeList.push_back(Snake(head,tail));
     }
     
+    //Get no of ladders and their details
     int ladderSize;
     vector<Ladder> ladderList;
     cout<<"How many ladders you want in your board?"<<endl;
@@ -86,10 +92,12 @@ int main(){
         ladderList.push_back(Ladder(bottom,top));
     }
 
+    //Initialize the board with snakes, ladders and dices
     Board board = Board(boardSize, snakeList, ladderList, diceCount);
     map<int, int> snakeMap = board.getSnakeMap();
     map<int, int> ladderMap = board.getLadderMap();
 
+    //Start the game
     int maxTurns =0;
     while(players.size() > 1){
         Player currentPlayer = players.front();
@@ -101,10 +109,12 @@ int main(){
         cout<<currentPlayer.getName()<<"'s position is "<<currentPosition<<" and dice value is "<<diceValue<<", so the expected position is "<<newPosition<<endl;
 
         if(newPosition <= boardMaxSize){
+            //Check if player got bit by a snake
             if(snakeMap.find(newPosition) != snakeMap.end()){
                 cout<<currentPlayer.getName()<<" got bit by a snake at "<<newPosition<<" and the got dropped to "<<snakeMap[newPosition]<<endl;
                 currentPlayer.setPosition(snakeMap[newPosition]);
             }
+            //check of player climbed a ladder
             else if(ladderMap.find(newPosition) != ladderMap.end()){
                 cout<<currentPlayer.getName()<<" got a ladder at "<<newPosition<<" and climbed to "<<ladderMap[newPosition]<<endl;
                 currentPlayer.setPosition(ladderMap[newPosition]);
@@ -122,6 +132,7 @@ int main(){
                     players.push(currentPlayer);
                 }
             }
+            //move ahead normally
             else{
                 currentPlayer.setPosition(newPosition);
                 if(newPosition == boardMaxSize){
@@ -138,18 +149,22 @@ int main(){
                     players.push(currentPlayer);
                 }
             }
-        }else{
+        }
+        //check if new position exceeds board limit 
+        else{
             cout<<currentPlayer.getName()<<" cannot move as new position exceeds the board limits!"<<endl;
             players.push(currentPlayer);
         }
         
         maxTurns++;
+        //stop game if total no of turns exceeds max turns allowed
         if(maxTurns > 100){
             cout<<"Total no of turns exceeds the max turns limit! Stopping the game!"<<endl;
             break;
         }
     }
 
+    //Print the winners
     for(int i =0 ; i<winners.size(); i++)
     {
         cout<<"Rank "<<i+1<< " : "<<winners[i].getName()<<endl;
